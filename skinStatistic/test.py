3b3cc76afbe75425c5f6b74fa5c67b0de61afc0a
VanTien PHAM <pvtien96@gmail.com>
2018-10-05 14:47:41 -0400

    New test.py file 06-10-2018
    by Tien

--- skinStatistic/test.py
+++ skinStatistic/test.py
@@ -40,14 +40,14 @@ def rGBAmountUpdateByComparing2Images(pngImage, jpgImage, frameHeight, frameWidt
 
 myPath = "/home/tien/OpenCV/Skin/Data/SegmentedData"
 
-print(myPath)
+#print(myPath)
 
 #pick an typical random image to get image's frame size; 1 time only for all others
 for root, directories, filenames in os.walk(myPath):
     if filenames:
         
         imgPath = os.path.join(root, filenames[0])
-        print(imgPath)
+        #print(imgPath)
         img = cv2.imread(imgPath)
         frameHeight = img.shape[0]
         frameWidth  = img.shape[1]
@@ -55,64 +55,93 @@ for root, directories, filenames in os.walk(myPath):
 print('Got frameSize')
 
 #start matching png vs jpg
-files = os.listdir(myPath)
-print(files)
-for f in files:
-    myNewPath = os.path.join(myPath, f)
-    # print (myNewPath)
-    fileOfUser = os.listdir(myNewPath) #Binh Hung Hoang
-    for fOU in fileOfUser:
-        myUserPath = os.path.join(myNewPath, fOU)
-        # print(myUserPath)
-        lastPath = os.listdir(myUserPath)
-        # print(lastPath)
+#myPath as level0Files
+
+level1Files = os.listdir(myPath)  #level1Files = ['Binh', 'Hung', 'Hoang']
+# print(level1Files)
+for level1File in level1Files: 
+    level1FilesPath = os.path.join(myPath, level1File)
+    # print (level1FilesPath) # level1FilesPath = /home/tien/OpenCV/Skin/Data/SegmentedData/Binh
+    level2Files = os.listdir(level1FilesPath) 
+    for level2File in level2Files:
+        level2FilesPath = os.path.join(level1FilesPath, level2File)
+        # print(level2FilesPath) # level2FilesPath = /home/tien/OpenCV/Skin/Data/SegmentedData/Binh/1
+        level3Files = os.listdir(level2FilesPath)
+        # print(level3Files) # level3Files = ['5 (3-19-2018 10-18-43 AM)', '1 (3-19-2018 10-18-37 AM)', '4.1', '3 (3-19-2018 10-18-39 AM)', '2 (3-19-2018 10-18-38 AM)', '1.1', '2.1', '5.1', '3.1', '4 (3-19-2018 10-18-41 AM)']
         
-        for i in lastPath:
-            if len(i) > 4:
+        for level3FileLabel in level3Files: # level3FileLabel = ['1.1']
+            if len(level3FileLabel) > 4:
                 continue
-            for j in lastPath:
-                if len(j) > 4 and i.split('.')[0] == j.split(' ')[0] :
-                    pngFinalPath = os.path.join(myUserPath, i)
-                    jpgFinalPath = os.path.join(myUserPath, j)
+            for level3FileOrigin in level3Files: # level3FileOrigin = ['1 (3-19-2018 10-18-37 AM)']
+                if len(level3FileOrigin) > 4 and level3FileLabel.split('.')[0] == level3FileOrigin.split(' ')[0] :
+                    pngFinalPath = os.path.join(level2FilesPath, level3FileLabel)
+                    jpgFinalPath = os.path.join(level2FilesPath, level3FileOrigin)
                     # print(pngFinalPath, " ", jpgFinalPath)
                     pngImages = os.listdir(pngFinalPath)
                     jpgImages = os.listdir(jpgFinalPath)
                     for jpgImage in jpgImages:
-                        xxx = jpgImage.split('.')[0]+'.png'
+                        matchedIndex = jpgImage.split('.')[0]+'.png' # matchedIndex = 3 11.pgn
+                        #print(matchedIndex)
                         pathJPG = os.path.join(jpgFinalPath, jpgImage)
-                        pathPNG = os.path.join(pngFinalPath, xxx)
+                        pathPNG = os.path.join(pngFinalPath, matchedIndex)
                         print("Working with: ", pathJPG, " and ", pathPNG)
-                        try:
                         
+                        try:
                             imageFromPathPNG = cv2.imread(pathPNG)
                             imageFromPathJPG = cv2.imread(pathJPG)                        
                             rGBAmountUpdateByComparing2Images(imageFromPathPNG, imageFromPathJPG, frameHeight, frameWidth)
                         except:
                             errorList.append(pathJPG)
 
+print('Sucessful Compared all matchable pairs PNG-JPG. Amount Updated! ErrorList (segmented errors) below: ')                        
+print(errorList)
+
 #red skin plot
-plt.subplot(131)
+plt.subplot(231)
 plt.title('Red Amount Plot')
+plt.xlabel('Pixel Value')
+plt.ylabel('Amount')
+plt.plot (rSkinAmount, 'r')
+
+#green skin plot
+plt.subplot(232)
+plt.title('Green Amount Plot')
+plt.xlabel('Pixel Value')
+plt.ylabel('Amount')
+plt.plot (gSkinAmount, 'g')
+
+#blue skin plot
+plt.subplot(233)
+plt.title('Blue Amount Plot')
+plt.xlabel('Pixel Value')
+plt.ylabel('Amount')
+plt.plot (bSkinAmount, 'b')
+
+#red vs non-red skin plot
+plt.subplot(234)
+plt.title('Red vs Non-Red Amount Plot')
 plt.xlabel('Pixel Value')
 plt.ylabel('Amount')
 plt.plot(rSkinAmount, 'r', rNotSkinAmount, 'k')
 #plt.show()
 
-#green skin plot
-plt.subplot(132)
-plt.title('Green Amount Plot')
+#green vs non-green skin plot
+plt.subplot(235)
+plt.title('Green vs Non-Green Amount Plot')
 plt.xlabel('Pixel Value')
 plt.ylabel('Amount')
 plt.plot(gSkinAmount, 'g', gNotSkinAmount, 'k')
 #plt.show()
 
-#blue skin plot
-plt.subplot(133)
-plt.title('Blue Amount Plot')
+#blue vs non-blue skin plot
+plt.subplot(236)
+plt.title('Blue vs Non-Blue Amount Plot')
 plt.xlabel('Pixel Value')
 plt.ylabel('Amount')
 plt.plot(bSkinAmount, 'b', bNotSkinAmount, 'k')
 
+#print(rSkinAmount)
+print("Program executed in %s seconds " %(time.time() - startTime))
+
 plt.show()
 
-print("Program executed in %s seconds " %(time.time() - startTime))
